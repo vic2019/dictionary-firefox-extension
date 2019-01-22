93fa1c4cbb7de85ba578196bd504a46bd23e8644
Victor <victor.fan.wang@gmail.com>
2019-01-21 20:16:13 -0800

    Switched from creating new window to appending new div

--- background.js
+++ background.js
@@ -1,30 +1,30 @@
-"use strict";
+// "use strict";
 
-function handleError(err) {
-  console.log(`Error: ${err}`);
+// function handleError(err) {
+//   console.log(`Error: ${err}`);
+// }
+
+function handleMessage(vocab) {
+  console.log(vocab);
 }
 
-function handleMessage(msg) {
-  selectedText = msg;
-}
-
-let selectedText = null;
+// let selectedText = null;
 
 browser.runtime.onMessage.addListener(handleMessage);
-browser.commands.onCommand.addListener((command) => {
-  if (command === "popup-window" && selectedText) {
+// browser.commands.onCommand.addListener((command) => {
+//   if (command === "popup-window" && selectedText) {
 
-    console.log(selectedText);
+//     console.log(selectedText);
 
-    browser.windows.create({
-      url: "public/popup_window.html",
-      type: "popup",
-      height: 300,
-      width: 300, 
-    })
-    .catch(handleError);
-  } 
-});
+//     browser.windows.create({
+//       url: "public/popup_window.html",
+//       type: "popup",
+//       height: 300,
+//       width: 300, 
+//     })
+//     .catch(handleError);
+//   } 
+// });
   
   
   
--- content_scripts.js
+++ content_scripts.js
@@ -1,20 +1,79 @@
-"user strict";
-
-function handleResponse(res) {
-  return;
-}
-
-function handleError(err) {
-  console.log(`Error: ${err}`);
-}
-
-function sendSelection() {
-  const selectedText = document.getSelection();
-  if (selectedText.isCollapsed) return null;
+function createPopup(event) {
+  const selection = window.getSelection();
+  if (selection.isCollapsed) return;
   
-  const sending = 
-      browser.runtime.sendMessage(selectedText.toString().trim());
-  sending.then(handleResponse, handleError);
+  const sendInfo = browser.runtime.sendMessage(
+    selection.toString().trim());
+  // sendInfo.then(updatePopup)
+  // .catch();
+
+  showLoadingPage(selection);
+
+
 }
 
-addEventListener("mouseup", sendSelection);
+function altSelect(event) {
+  if (event.altKey) createPopup(event);
+}
+
+function showLoadingPage(selection) {
+  const selectionCoords = selection.getRangeAt(0).getBoundingClientRect();
+  
+  const popupDiv = document.createElement('div');
+  popupDiv.className = 'wordiePopup';
+  
+  popupDiv.innerHTML = "<p> blah <br> <br> blah blah</p>"
+  popupDiv.style.background = '#7fcfcf';
+  popupDiv.style.position = 'fixed';
+  document.body.append(popupDiv);
+  
+  let top = selectionCoords.top - popupDiv.offsetHeight - 4;
+  if (top < 0) {
+    top = selectionCoords.bottom + 4;
+  }
+  alert(`select.top: ${selectionCoords.top},\npopup.offsetHeight: ${popupDiv.offsetHeight},\ntop: ${top}`);
+  
+  let left = selectionCoords.right + 4;
+  if (left + popupDiv.offsetWidth > document.documentElement.clientWidth) {
+    left = document.documentElement.clientWidth - popupDiv.offsetWidth - 2;
+  }
+  // alert(typeof popupDiv.offsetHeight);
+  // alert(typeof selectionCoords.top);
+  popupDiv.style.left = left + 'px';
+  popupDiv.style.top = top + 'px';
+}
+
+
+addEventListener("dblclick", createPopup);
+addEventListener("mouseup", altSelect);
+
+// "user strict";
+
+// function handleResponse(res) {
+//   return;
+// }
+
+// function handleError(err) {
+//   console.log(`Error: ${err}`);
+// }
+
+// function sendSelection() {
+//   const selectedText = document.getSelection();
+//   if (selectedText.isCollapsed) return null;
+  
+//   const sending = 
+//       browser.runtime.sendMessage(selectedText.toString().trim());
+//   sending.then(handleResponse, handleError);
+// }
+
+
+// content
+// 	altMouseup
+// 	createPopup
+//    sendInfo
+// 		showLoading
+// 	  updatePopup
+//  
+// 	
+// 	removePopup (click)
+// 	saveEntry
