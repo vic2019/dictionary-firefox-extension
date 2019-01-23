18b1f6ea9ebc2b0ccacf31b3873d8cdfa22b44cc
Victor <victor.fan.wang@gmail.com>
2019-01-22 17:33:50 -0800

    Make popup div switch sides when running out of space

--- content_scripts.js
+++ content_scripts.js
@@ -9,25 +9,55 @@ function createPopup(event) {
   // sendInfo.then(updatePopup)
   // .catch();
 
-  showPopup(selection);
+  const popupDiv = setPopup(selection);
+
 }
 
-function showPopup(selection) {
+function setPopup(selection) {
+  // Set anchor 
   const selectionCoords = selection.getRangeAt(0).getBoundingClientRect();
   const anchorCoords = getAnchorCoords(selectionCoords);
   
+  // Create popup
   const popupDiv = document.createElement('div');
+  const popupHeight = 370;
+  const popupWidth = 330;
   popupDiv.className = 'wordiePopup';
-  // popupDiv.style.width = '300px';
-  popupDiv.style.top = anchorCoords.top + 'px';
-  popupDiv.style.left = anchorCoords.left + 'px';
+  popupDiv.style.padding = "20px 30px";
+  popupDiv.style.border = "2px solid #8cd9b3";
+  popupDiv.style.borderRadius = "9px";
+  popupDiv.style.textAlign = "center";
+  popupDiv.style.background = '#d9f2e6';
+  popupDiv.style.boxShadow = "3px 5px 6px rgba(0, 0, 0, .1)";
+  popupDiv.style.height = popupHeight + 'px';
+  popupDiv.style.width = popupWidth + 'px';
+  
+  // Set popup position
+  // * Need to figure out how to get precise height & width *
+  let top = anchorCoords.top - 410 - 15;
+  let left = anchorCoords.left + 
+  (anchorCoords.right - anchorCoords.left - 390 ) / 2;
+  
+  if (top < pageYOffset) {
+    top = anchorCoords.bottom + 15;
+  }
+  
+  if (left < pageXOffset) {
+    left = pageXOffset;
+  }
+
+  if (left > document.documentElement.clientWidth - 390) {
+    left = document.documentElement.clientWidth - 390;
+  }
+
+  popupDiv.style.top = top + 'px';
+  popupDiv.style.left = left + 'px';
   popupDiv.style.position = 'absolute';
   popupDiv.style.zIndex = 1000;
-  popupDiv.style.hidden = false;
-  popupDiv.innerHTML = "<p>Searching...</p>"
-  popupDiv.style.background = '#7fcfcf';
-  document.body.append(popupDiv);
 
+  document.body.append(popupDiv);
+  
+  return popupDiv;
 }
     
 function getAnchorCoords(selectionCoords) {
