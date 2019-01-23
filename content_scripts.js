b7f77f9c1a1be50eea880f658d2bd1152e19012a
Victor <victor.fan.wang@gmail.com>
2019-01-23 06:08:53 -0800

    Added removePopup

--- content_scripts.js
+++ content_scripts.js
@@ -20,8 +20,7 @@ function setPopup(selection) {
   
   // Create popup
   const popupDiv = document.createElement('div');
-  const popupHeight = 370;
-  const popupWidth = 330;
+
   popupDiv.className = 'wordiePopup';
   popupDiv.style.padding = "20px 30px";
   popupDiv.style.border = "2px solid #8cd9b3";
@@ -29,25 +28,28 @@ function setPopup(selection) {
   popupDiv.style.textAlign = "center";
   popupDiv.style.background = '#d9f2e6';
   popupDiv.style.boxShadow = "3px 5px 6px rgba(0, 0, 0, .1)";
-  popupDiv.style.height = popupHeight + 'px';
-  popupDiv.style.width = popupWidth + 'px';
+  popupDiv.style.height = '370px';
+  popupDiv.style.width = '330px';
+  document.body.append(popupDiv);
   
   // Set popup position
   // * Need to figure out how to get precise height & width *
-  let top = anchorCoords.top - 410 - 15;
+  let top = anchorCoords.top - popupDiv.offsetHeight - 15;
   let left = anchorCoords.left + 
-  (anchorCoords.right - anchorCoords.left - 390 ) / 2;
+  (anchorCoords.right - anchorCoords.left - popupDiv.offsetWidth ) / 2;
   
   if (top < pageYOffset) {
     top = anchorCoords.bottom + 15;
   }
   
-  if (left < pageXOffset) {
-    left = pageXOffset;
+  if (left < pageXOffset + 10) {
+    left = pageXOffset + 10;
   }
 
-  if (left > document.documentElement.clientWidth - 390) {
-    left = document.documentElement.clientWidth - 390;
+  if (left > document.documentElement.clientWidth - 
+    popupDiv.offsetWidth - 10) {
+    left = document.documentElement.clientWidth - 
+    popupDiv.offsetWidth - 10;
   }
 
   popupDiv.style.top = top + 'px';
@@ -55,7 +57,6 @@ function setPopup(selection) {
   popupDiv.style.position = 'absolute';
   popupDiv.style.zIndex = 1000;
 
-  document.body.append(popupDiv);
   
   return popupDiv;
 }
@@ -74,13 +75,22 @@ function getAnchorCoords(selectionCoords) {
   };
 }
 
-document.addEventListener("dblclick", createPopup);
+function removePopup(event) {
+  if (!event.target.classList.contains('wordiePopup')) {
+    document.querySelectorAll('.wordiePopup').forEach((elem) => {
+      elem.remove();
+    });
+  }
+}
 
-// content
-// 	createPopup
-//    sendInfo
-// 		createDiv
+document.addEventListener("dblclick", createPopup);
+document.addEventListener("click", removePopup);
+
+// ~content
+// 	~createPopup
+//    ~sendInfo
+// 		~createDiv
 // 	  updatePopup
 //  
-// 	removePopup (click)
+// 	~removePopup (click)
 // 	saveEntry
