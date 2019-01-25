403fe30983703bf5f52b42f21b514f5711dee28d
Victor <victor.fan.wang@gmail.com>
2019-01-24 16:20:37 -0800

    Attempted to build DOM tree by parsing the response JSON

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 "use strict";
 
 // function handleError(err) {
-//   console.log(`Error: ${err}`);
+//   console.log(err);
 // }
 
 function lookUpWord(word) {
   const KEY = '';
-  const requestUrl = `https://www.dictionaryapi.com/api/v3/references/learners/json/${word}?key=${KEY}`;
-  console.log(requestUrl);
+  const requestUrl = `http://www.dictionaryapi.com/api/v3/references/learners/json/${word}?key=${KEY}`;
+  // console.log(requestUrl);
 
   const httpRequest = new XMLHttpRequest();
   if (!httpRequest) return false;
@@ -18,37 +18,51 @@ function lookUpWord(word) {
 }
 
 function handleResponse() {
-  if (this.readyState === XMLHttpRequest.DONE) {
-    if (this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      console.log(response);
-
-      // console.log(response.hwi.hw);
-      // console.log(response.shortdef[0]);
-
-    } else {
-      console.log(this.status);
-    }
-  } else {
-    console.log(this.readyState.toString());
+  if (this.readyState !== XMLHttpRequest.DONE) return;
+  if (this.status !== 200) {
+    console.log(this.status);
+    return;
   }
   
+  const response = JSON.parse(this.responseText);
+  // console.log(typeof response);
+      
+  buildDOM(response);
+
+}
+
+browser.runtime.onMessage.addListener(lookUpWord);
+
+
+function buildDOM(response) {
+  // Check if the word was found
+  if (typeof response[0] === 'string') return;
+
+  const content = document.createElement('div');
+
+  // Filter out irrelevant entries
+  for (let entry of response) {
+    if (entry.hwi.hw !== word) continue;
+    
+    content.heading = getHeading(entry);
+    content.body = getBody(entry);
+  } 
+}
+
+function getHeading(entry) {
+  alert('heading');
+
+}
+  // Process hwi (hw, prs), fl, gram
+  
+  // Process def -> sseq. Go through ea, ch sense. Get sn, dt, vis, t
+  
+  // Process uros as "more examples". Process all the vis -> t.
+
+function getBody(entry) {
+  alert('body');
 }
 
 
 
 
-
-
-browser.runtime.onMessage.addListener(lookUpWord);
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
