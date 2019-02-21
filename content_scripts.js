a71c7a287a5aa923f6ed3979a78ea408e7811f24
Victor <victor.fan.wang@gmail.com>
2019-02-21 12:14:14 -0800

    Added audio

--- content_scripts.js
+++ content_scripts.js
@@ -9,6 +9,7 @@ function createPopup() {
   
   const word = selection.toString().trim();
   sendRequest(word); 
+
 }
 
 
@@ -22,6 +23,7 @@ function sendRequest(word) {
   httpRequest.onreadystatechange = handleResponse;
   httpRequest.open('GET', requestUrl);
   httpRequest.send(); 
+
 }
 
 
@@ -35,11 +37,12 @@ function handleResponse() {
   //Need to check whether the word is available first
   const content = parseXmlObject(xmlDoc.getElementsByTagName('entry_list')[0]);
   updatePopup(content);
+
 }
 
 
 function parseXmlObject(xmlNode) {
-  const selfElem = document.createElement('div');
+  const selfElem = document.createElement('span');
   selfElem.className = xmlNode.tagName;
 
   if (!xmlNode.hasChildNodes()) return selfElem;
@@ -53,6 +56,7 @@ function parseXmlObject(xmlNode) {
   }
 
   return selfElem;  
+
 }
 
 
@@ -62,120 +66,200 @@ function updatePopup(content) {
   popupDiv.innerHTML = '';
   popupDiv.style.textAlign = 'left';
   popupDiv.style.lineHeight = 1.2;
-  popupDiv.append(content);     
+  popupDiv.append(content);
+
   
   const css = {
+    entry: `
+    display: block;
+    `,
     hw: `
-      padding-top: 2%;
-      padding-bottom: 2%;
-      font-size: 130%; 
-      font-weight: bold;
-      display: inline;
+    padding-top: 2%;
+    padding-bottom: 2%;
+    padding-right: 1%;
+    font-size: 130%; 
+    font-weight: bold;
+    display: inline-block;
+    `,
+    vr: `
+    display: none;
     `,
     sound: `
-      display: none;
+    display: none;
     `,
     pr: `
-      display: none;
+    display: none;
     `,
     fl: `
-      color: #787878;
-      padding-top: 1%;
-      padding-bottom: 1%;
-      font-style: italic;
-      display: inline;
+    color: #787878;
+    font-weight: bold;
+    font-style: italic;
     `,
-    in: `
-      padding-top: 2%;
-      padding-bottom: 1%;
+    lb: `
+    color: #787878;
     `,
-    dt: `
-      display: inline;
-      padding-top: 1%;
-      padding-bottom: 1%;
-    `,
-    il: `
-      color: #787878;
-      font-style: italic;
-      display: inline;
-    `,
-    if: `
-      display: inline;
-    `,
-    sn: `
-      display: inline;
-      font-weight: bold;
-    `,
-    wsgram: `
-      color: #787878;
-      padding-top: 2%;
-      padding-bottom: 1%;
-      text-transform: uppercase;
-      font-size: 75%;
-      font-weight: bold;
-    `,
-    vi: `
-      display:list-item;
-      list-style: square inside;
-      padding-left: 5%;
-      padding-top: 1%;
-      padding-bottom: 1%;
-    `,
-    it: `
-      font-style: italic;
-      display: inline;
-    `,
-    un: `
-      padding-top: 1%;
-      padding-bottom: 1%;
-      display: inline;
-      display:list-item;
-      list-style: circle inside;
-    `,
-    snote: `
-      display: inline;
-      display:list-item;
-      list-style: circle inside;
-    `,
-    phrase: `
-      font-style: italic;
-      display: inline;
-    `,
-    dro: `
-      display: none;
-    `,
-    dx: `
-      display: none;
-    `,
-    usage: `
-      padding-top: 1%;
-      padding-bottom: 1%;
-      display: inline;
-      display:list-item;
-      list-style: circle inside;
+    hsl: `
+    color: #787878;
+    display: inline;
     `,
     ssl: `
-      color: #787878;
-      padding-top: 1%;
-      padding-bottom: 1%;
-      font-style: italic;
-      display: inline;
+    color: #787878;
+    font-style: italic;
+    `,
+    sl: `
+    color: #787878;
+    font-style: italic;
+    `,
+    sin: `
+    display: block;
+    `,
+    sn: `
+    font-weight: bold;
     `,
     sgram: `
-      color: #787878;
-      padding-top: 1%;
-      padding-bottom: 1%;
-      font-style: italic;
-      display: inline;
+    color: #787878;
+    font-style: italic;
+    `,
+    slb: `
+    color: #787878;
+    `,
+    in: `
+    color: #787878;
+    display: block;
+    `,
+    il: `
+    color: #787878;
+    font-style: italic;
+    `,
+    cx: `
+    padding-top: 1%;
+    padding-bottom: 1%;
+    display: block;
+    `,
+    ct: `
+    font-style: italic;
+    `,
+    dt: `
+    padding-left: 1%;
+    `,
+    def: `
+    padding-top: 1%;
+    padding-bottom: 1%;
+    display: block;
+    `,
+    vt: `
+    color: #787878;
+    padding-top: 2%;
+    padding-bottom: 1%;
+    text-transform: uppercase;
+    font-size: 75%;
+    font-weight: bold;  
+    display: block;
+    `,
+    wsgram: `
+    color: #787878;
+    padding-top: 2%;
+    padding-bottom: 1%;
+    text-transform: uppercase;
+    font-size: 75%;
+    font-weight: bold;
+    display: block;
+    `,
+    gram: `
+    color: #787878;
+    padding-top: 1%;
+    padding-bottom: 1%;
+    display: block;
+    `,
+    vi: `
+    display:list-item;
+    list-style: square inside;
+    padding-left: 5%;
+    padding-top: 1%;
+    padding-bottom: 1%;
+    `,
+    it: `
+    font-style: italic;
+    `,
+    un: `
+    display:list-item;
+    list-style: circle inside;
+    padding-top: 1%;
+    padding-bottom: 1%;
+    `,
+    snote: `
+    display:list-item;
+    list-style: circle inside;
+    `,
+    phrase: `
+    font-style: italic;
+    `,
+    usage: `
+    display:list-item;
+    list-style: circle inside;
+    `,
+    dro: `
+    display: none;
+    `,
+    usageref: `
+    display: none;
+    `,
+    altpr: `
+    display: none;
+    `,
+    snotebox: `
+    display: none;
+    `,
+    art: `
+    display: none;
     `
-
   }
   
   for (let className of Object.keys(css)) {
     Array.from(popupDiv.getElementsByClassName(className)).forEach(
       elem => elem.style = css[className]
     ); 
-  } 
+  }
+  
+  Array.from(popupDiv.getElementsByClassName('entry')).forEach( entry => {
+    const audio = entry.getElementsByClassName('sound')[0].firstChild;
+    const audioElem = getAudio(audio);
+    entry.getElementsByClassName('hw')[0].after(audioElem);
+
+    const playButton = document.createElement('img');
+    playButton.src = browser.extension.getURL("images/play_button.png");
+    playButton.onclick = playAudio(audioElem);
+    entry.getElementsByClassName('hw')[0].after(playButton);
+  });
+
+}
+
+
+function getAudio(audio) {
+  const fileName = audio.innerHTML;
+  let subdir = fileName.slice(0, 1);
+
+  if (fileName.slice(0, 3) === 'bix') {
+    subdir = 'bix';
+  } else if (fileName.slice(0, 2) === 'gg') {
+    subdir = 'gg';
+  } else if (fileName.slice(0, 1).match(/\d/)) {
+    subdir = 'number';
+  }
+
+  const src = `https://media.merriam-webster.com/soundc11/${subdir}/${fileName}`;
+  const audioElem = document.createElement('audio');
+  audioElem.className = fileName;
+  audioElem.src = src;
+  audioElem.display = 'none';
+  
+  return audioElem;
+
+}
+
+
+function playAudio(audioElem) {
+  return () => audioElem.play();
 }
 
 
@@ -194,7 +278,7 @@ function showPopup(selection) {
   popupDiv.style.margin = "0px";
   popupDiv.style.padding = "10px 16px";
   popupDiv.style.height = '380px';
-  popupDiv.style.width = '350px';
+  popupDiv.style.width = '360px';
   popupDiv.style.boxShadow = "3px 5px 6px rgba(0, 0, 0, .1)";
   popupDiv.style.textAlign = 'center';
   popupDiv.style.lineHeight = 25;
@@ -203,8 +287,8 @@ function showPopup(selection) {
   popupDiv.innerHTML = 'Searching...'
 
   // Set popup position
-  const offsetHeight = 408;
-  const offsetWidth = 386;
+  const offsetHeight = 404;
+  const offsetWidth = 396;
   const scrollHeight = document.documentElement.scrollHeight;
   const clientWidth = document.documentElement.clientWidth;
   
@@ -232,8 +316,9 @@ function showPopup(selection) {
   popupDiv.style.position = 'absolute';
   popupDiv.style.zIndex = 1000;
   document.body.append(popupDiv);
-  
+
   return popupDiv;
+
 }
 
 
@@ -249,15 +334,20 @@ function getAnchorCoords(selectionCoords) {
     left: left,
     right: right,
   };
+
 }
 
 
 function removePopup(event) {
-  if (!event.target.classList.contains('wordiePopup')) {
-    document.querySelectorAll('.wordiePopup').forEach((elem) => {
-      elem.remove();
-    });
+  const popupDivs = document.querySelectorAll('.wordiePopup');
+  for (popupDiv of popupDivs) {
+    if(popupDiv.contains(event.target)) return;
   }
+
+  document.querySelectorAll('.wordiePopup').forEach((elem) => {
+    elem.remove();
+  });
+
 }
 
 
