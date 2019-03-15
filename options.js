683f4b1437f8d1aa9a9c4575b27d35b13efbcf7f
Victor <victor.fan.wang@gmail.com>
2019-03-15 05:26:23 -0700

    Edited css

--- options.js
+++ options.js
@@ -1,14 +1,14 @@
 document.querySelector('#setKey').addEventListener('click', saveKey);
 document.querySelector('#default').addEventListener('click', () => {
   browser.storage.local.set({key: undefined});
-  alert('Demo key saved!');
+  window.alert('Demo key saved!');
 });
 
 function saveKey(event) {
   const key = document.getElementById('api-key').value.trim();
 
   if (!key) {
-    alert('Please enter a valid key.');
+    window.alert('Please enter a valid key.');
     return;
   }
 
